98a6e9ac296fc003dc58a1a6bb22c118ee4c202d
mao1207 <mljdyx0531@163.com>
2022-05-15 23:42:25 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-7gp4z0p12b11f8b0","alias":"cloud1"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\applet of WeChat\微信web开发者工具\cli.bat" cloud functions deploy --e cloud1-7gp4z0p12b11f8b0 --n quickstartFunctions --r --project "D:\applet of WeChat\济时代\济时代项目" --report_first --report
